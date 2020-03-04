3d98b2e15b38fe979c332770aa37b57fa93a5dc4
belovedparvez <48083985+belovedparvez@users.noreply.github.com>
2020-03-04 15:07:04 +0530

    mechsComp

---
+++ CircusGame/Assets/Mechanics/Scripts/EquipManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EquipManager : MonoBehaviour
+{
+    private Dictionary<BodyPartType, BodyPart> bodyParts = new Dictionary<BodyPartType, BodyPart>();
+    public List<Item> inventory = new List<Item>();
+
+    public Equipable starting_item;
+
+    public float strength = 10;
+    public float mana = 10;
+
+    void Start()
+    {
+        BodyPart[] get_parts = transform.GetComponentsInChildren<BodyPart>();
+        foreach(var part in get_parts) {
+            bodyParts.Add(part.type, part);
+            part.manager = this;
+        }
+
+        EquipItem(starting_item);
+    }
+
+    void Pickup(Pickable pick) {
+        if (!pick)
+            return;
+        Debug.Log(pick.name);
+        var item = pick.item;
+        if(item.GetType() == typeof(Equipable)) {
+            EquipItem((Equipable)item);
+        } else {
+            inventory.Add(item);
+        }
+        pick.PickUp();
+    }
+
+    void EquipItem(Equipable item) {
+        if (!item)
+            return;
+        if(bodyParts.TryGetValue(item.type, out var part)) {
+            part.Equip(item);
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F)) {
+            Pickup(Pickable.GetItem(transform.position, 10));
+        }
+    }
+}
---
+++ CircusGame/Assets/Mechanics/Scripts/Rope/RopeSetter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RopeSetter : MonoBehaviour
+{
+    public SpiderRope[] rope;
+    private int index = 0;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            index = index > rope.Length - 1 ? 0 : index;
+            Vector2 worldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            rope[index].setStart(worldPos);
+            index++;
+        }
+    }
+}
